2412d599f951de148631719ce064be36e8a17f4c
Dany Cerone <danycerone@mbp-de-dany.einet.ad.eivd.ch>
2017-03-17 11:16:15 +0100

    ajout des fichiers pour le commit précédent qui n'a pas bien marché

--- www/js/single.js
+++ www/js/single.js
@@ -100,14 +100,47 @@ angular.module('citizen-engagement').controller('SingleCtrl', function($statePar
      singleCtrl.comment ="";
      return res.data;
 
-    }).catch(function(error) {
-      singleCtrl.error = error;
+    }).catch(function() {
+      singleCtrl.comment.error = "Please you have to add some content to your comment";
       // If an error occurs, hide the loading message and show an error message.
-      console.log("error, plaese put error content");
+      
       
     });
     
   }
+
+  singleCtrl.createIssue = function(){
+      // requete get
+      // creation du resultat ou pas?
+
+        
+      return $http({
+        method: 'POST',
+        url: apiUrl+'/issues/'+$scope.issue.id+'/comments',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data: {"text": singleCtrl.comment},
+        params: {include: 'author'}
+        
+      }).then(function(res) {
+
+        // If successful, give the token to the authentication service.
+       
+       $scope.comments.push(res.data);
+       console.log(res);
+       singleCtrl.comment ="";
+       return res.data;
+
+      }).catch(function() {
+        singleCtrl.comment.error = "Please you have to add some content to your comment";
+        // If an error occurs, hide the loading message and show an error message.
+        
+        
+      });
+      
+  }
+
   function getCurrentUser(){
     // requete get
       
